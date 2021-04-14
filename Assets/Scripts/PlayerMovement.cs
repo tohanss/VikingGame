f12955d15de408735b70cbb7ee626b535339a4b9
tohanss <tobihanobi@gmail.com>
2021-04-15 00:44:31 +0200

    [feature] Add ranged attack
    Fix #7. Add collision with enemies in #6

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,28 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movespeed = 5.0f;
     public Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
     Vector2 movement;
+
+    private void Start() 
+    {
+        spriteRenderer = transform.GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        } 
     }
 
     void FixedUpdate()
