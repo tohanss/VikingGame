5a1d07647b9f8d28270385824dac8bb0527a9e6a
Gabriel Chang <gc.chang95@gmail.com>
2021-04-15 12:24:58 +0200

    Implement projectile functions
    Projectile can now do damage to objects with "enemy" tag and gets destroyed on collision with enemy
    Projectile has max life duration

--- Assets/Scripts/SpearProjectile.cs
+++ Assets/Scripts/SpearProjectile.cs
@@ -6,11 +6,35 @@ public class SpearProjectile : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
+    public float damage = 5f;
+
+    private float maxLife = 4.0f; //temporary solution, max life duration for projectile 
+    private float lifeTimer;
 
     void Start()
     {
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         
         rb.velocity = direction.normalized * speed;
+    }
+
+    private void Update()
+    {
+        lifeTimer += Time.deltaTime;
+        if (lifeTimer >= maxLife) 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy")) 
+        {
+            other.GetComponent<Enemy>().takeDamage(damage);
+            Destroy(gameObject);
+
+        }
+
     }
 }
