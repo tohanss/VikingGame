fcef67233619ff301e35fc91f8c8a7356521a9b3
Gabriel Chang <gc.chang95@gmail.com>
2021-04-15 14:34:04 +0200

    add basic enemy movement
    Implemented enemy movement
    Enemy has a aggroRange and attackRange
    Enemy will move towards player when in aggroRange and stop when in attackRange

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,23 +10,47 @@ public class Enemy : MonoBehaviour
     private float health;
 
     public Transform target;
+    public float aggroRange;
+    public float attackRange;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         health = maxHealth;
+        spriteRenderer = transform.GetComponent<SpriteRenderer>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
     }
 
     private void Update()
     {
+        
         if (health <= 0)
         {
             die();
         }
+
+        //Enemy faces player by fliping the sprite
+        if (transform.position.x < target.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        move();
+
     }
 
     private void move()
     {
-        
+        if (Vector2.Distance(transform.position, target.position) <= aggroRange && Vector2.Distance(transform.position, target.position) > attackRange) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+
     }
 
     private void attack() 
@@ -43,4 +67,5 @@ public class Enemy : MonoBehaviour
     {
         health -= damage;
     }
+
 }
