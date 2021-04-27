4fea686e744cc83357dc124028f2bbd465943787
Gabriel Chang <gc.chang95@gmail.com>
2021-04-27 15:53:52 +0200

    Fix colliders for boar and player
    Fix colliders for player and boar prefabs. Move out player feetcollider into a childobject(feetCollider) and added a new boxcollider for the player.
    Now the player can get hit.
    Made so the boar attack continues after hitting player, the player can only get hit once per attack.
    Added PlayerFeet and EnemyFeet layers for their feetcolliders. Different types of feet shall now ignore each other.

--- Assets/Scripts/Enemies/Boar.cs
+++ Assets/Scripts/Enemies/Boar.cs
@@ -11,19 +11,16 @@ public class Boar : Enemy
     private float lastTime = 0;
     private float attackCooldown = 3.0f;
     private float lastAttackStartTime;
-
     // Misc
     private Vector3 targetLastPos;
     private Vector3 direction;
-
-
+    private Collider2D hitPlayer;
 
     protected override void attack()
     {
-
         if (!isAttacking)
         {
-            targetLastPos = target.position;
+            targetLastPos = target.position + new Vector3(0, 0.5f, 0); // This so boar doesen't target feet rather targets the center of player
             direction = (targetLastPos - transform.position).normalized;
             lastAttackStartTime = Time.time;
 
@@ -40,16 +37,18 @@ public class Boar : Enemy
         {
             lastTime = Time.time;
             isAttacking = false;
+            hitPlayer = null;
         }
     }
-    //Handles player taking damage and stopping the attack if attack hits
+    //Handles player taking damage and player can't get hit more than once per attack
     private void OnTriggerEnter2D(Collider2D other)
     {   
-        if (other.gameObject.CompareTag("Player") && isAttacking)
+        if (other.gameObject.CompareTag("Player") && isAttacking && !(hitPlayer == other))
         {
-            isAttacking = false;
+            //isAttacking = false;
+            hitPlayer = other;
             Debug.Log("Player take 20 damage");
-            lastTime = Time.time;
+            //lastTime = Time.time;
         }
     }
 
