9ff690f3a3f7839dd30996e5d64eddb77eff2cc1
Gabriel Chang <gc.chang95@gmail.com>
2021-04-27 14:27:37 +0200

    Refactor enemy script using inheritance and add boar attack
    Created a boar script that inherits from enemies.
    From now on when we create enemy script it should inherit from "Enemy".
    Enemies will now start moving towards player when taken damage even if they are outside of aggro range.
    Added boar charge attack

---
+++ Assets/Scripts/Enemies/Boar.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boar : Enemy
+{
+    //Boar specific stats
+    public float chargeSpeed;
+    private float chargeDuration = 4.0f; //how long to keep charging
+    // Cooldown related
+    private float lastTime = 0;
+    private float attackCooldown = 2.0f;
+    // Misc
+    private Vector3 targetLastPos;
+    private Vector3 direction;
+
+
+
+    protected override void attack()
+    {
+
+        if (!isAttacking)
+        {
+            targetLastPos = target.position;
+            direction = (targetLastPos - transform.position).normalized;
+
+        }
+        //if cooldown has passed, do charge attack in the direction of the player when the attack started
+        if (Time.time - lastTime > attackCooldown)
+        {
+            isAttacking = true;
+            transform.position = Vector2.MoveTowards(transform.position, transform.position + direction, chargeSpeed * Time.deltaTime);
+
+        }
+        //Stop charge attack when a certain ammount of time has passed
+        if (Time.time - lastTime >= chargeDuration)
+        {
+            lastTime = Time.time;
+            isAttacking = false;
+        }
+    }
+    //Handles player taking damage and stopping the attack if attack hits
+    private void OnTriggerEnter2D(Collider2D other)
+    {   
+        if (other.gameObject.CompareTag("Player") && isAttacking)
+        {
+            isAttacking = false;
+            Debug.Log("Player take 20 damage");
+            lastTime = Time.time;
+        }
+    }
+
+}
