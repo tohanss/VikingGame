5d51af347aa3aaf1a13311f1dfbcb97b2c07215f
Gabriel Chang <gc.chang95@gmail.com>
2021-05-05 17:37:25 +0200

    add some more code to script and adjustment to prefab

--- Assets/Scripts/Enemies/Troll.cs
+++ Assets/Scripts/Enemies/Troll.cs
@@ -7,16 +7,44 @@ public class Troll : Enemy
     //Troll specific stats
     private float slamDamage;
     // Misc
-
+    private Vector3 targetLastPos;
+    private Vector3 direction;
 
     protected override void Start()
     {
         base.Start();
-        attackCooldown = 3.0f;
+        attackCooldown = 2.0f;
     }
     protected override void attack()
     {
-        base.attack();
+        if (!isAttacking)
+        {
+            targetLastPos = target.position; 
+            direction = (targetLastPos - transform.position).normalized;
+            lastAttackStartTime = Time.time;
+
+        }
+        if (Time.time - lastTime > attackCooldown)
+        {
+            isAttacking = true;
+        }
+        else 
+        {
+            isAttacking = false;
+
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && isAttacking)
+        {
+            if (!other.GetComponent<PlayerActions>().isInvulnerable) //only do damage if player is not invulnerable
+            {
+                other.GetComponent<PlayerActions>().playerTakeDamage(slamDamage);
+            }
+        }
     }
 
 }
