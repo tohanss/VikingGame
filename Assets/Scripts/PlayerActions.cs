44f7a3d07cae86f86229a65874498ab37c6f32eb
stjerndale <stjernda@kth.se>
2021-05-06 19:28:09 +0200

    Revert "Revert "[feature] Issue/105 UI hp xp bars""

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using TMPro;
 
 public class PlayerActions : MonoBehaviour
 {
@@ -15,14 +16,21 @@ public class PlayerActions : MonoBehaviour
 
     // Player stats
     private int level = 1;
+    private float maxHealth;
     private float currentHealth;
     public float movespeed;
 
     // Experience and level related
     private int currentExp = 0;
     private int requiredExp = 10;
-    public Text favorText;
-    public Text levelText;
+
+    // UI related
+    public Canvas canvas;
+    private Image HPbar;
+    private TMP_Text HPcurrent;
+    private TMP_Text HPmax;
+    private Image XPbar;
+    private TMP_Text levelText;
 
     // Hidden variables in inspector
     [HideInInspector]
@@ -40,14 +48,26 @@ public class PlayerActions : MonoBehaviour
     // Unity functions
     private void Start() 
     {
-        favorText.text = "Favour: " + currentExp.ToString() + " / " + requiredExp.ToString();
-        levelText.text = "Level: " + level.ToString();
-
         playerClass = GetComponent<PlayerClass>();
         playerRB = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = transform.GetComponent<SpriteRenderer>();
-        currentHealth = playerClass.maxHealth;
+        maxHealth = playerClass.maxHealth;
+        currentHealth = maxHealth;
+
+        // Set up UI
+        // HP related UI
+        HPbar = canvas.transform.GetChild(1).GetChild(1).GetComponent<Image>();
+        HPcurrent = canvas.transform.GetChild(1).GetChild(3).GetComponent<TMP_Text>();
+        HPmax = canvas.transform.GetChild(1).GetChild(5).GetComponent<TMP_Text>();
+        HPbar.fillAmount = 1;
+        HPmax.text = currentHealth.ToString();
+        HPcurrent.text = currentHealth.ToString();
+        // XP related
+        XPbar = canvas.transform.GetChild(2).GetChild(1).GetComponent<Image>();
+        levelText = canvas.transform.GetChild(2).GetChild(3).GetComponent<TMP_Text>();
+        XPbar.fillAmount = 0f;
+        levelText.text = "Lv. " + level;
     }
 
     private void Update()
@@ -62,14 +82,14 @@ public class PlayerActions : MonoBehaviour
     }
 
     // Level and EXP related
-    public void GainExp(int exp) 
+    public void GainExp(int exp)
     {
         currentExp += exp;
+        XPbar.fillAmount = (float) currentExp / (float) requiredExp;
+
         if (currentExp >= requiredExp){
             LevelUp();
         }
-
-        favorText.text = "Favour: " + currentExp.ToString() + " / " + requiredExp.ToString();
     }
 
     private void LevelUp()
@@ -83,8 +103,9 @@ public class PlayerActions : MonoBehaviour
         currentExp -= requiredExp;
         requiredExp += level * 5;
 
-        // Change level text
-        levelText.text = "Level: " + level.ToString();
+        // Update UI for new level
+        levelText.text = "Lv. " + level.ToString();
+        XPbar.fillAmount = currentExp / requiredExp;
     }
 
     public void playerTakeDamage(float damage)
@@ -93,6 +114,9 @@ public class PlayerActions : MonoBehaviour
         
         Debug.Log("Damage Taken: " + damage);
         currentHealth -= damage;
+        HPcurrent.text = currentHealth.ToString();
+        HPbar.fillAmount = currentHealth / maxHealth;
+
         if (currentHealth <= 0)
         {
             die();
@@ -103,9 +127,9 @@ public class PlayerActions : MonoBehaviour
     {
         currentHealth += amount;
 
-        if (currentHealth > playerClass.maxHealth)
+        if (currentHealth > maxHealth)
         {
-            currentHealth = playerClass.maxHealth;
+            currentHealth = maxHealth;
         }
 
         TextMesh damageNumber = Instantiate(damageNumbers, transform.position, Quaternion.identity).transform.GetChild(0).GetComponent<TextMesh>();
