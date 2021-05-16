fa950ff084cb179fff710ad25d126eab2f26e590
Gabriel Chang <gc.chang95@gmail.com>
2021-05-16 16:50:40 +0200

    Add tutorial and defeat screen
    After title screen there will be tutorial text on the controls
    The text will disappear if player uses portal
    Now if the player dies there will be a defeat screen that resets the game after a while

---
+++ Assets/Scripts/TutorialText.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TutorialText : MonoBehaviour
+{
+    [SerializeField] private TMP_Text tutorialText;
+    [SerializeField] private GameObject tutorialRoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(changeText());
+    }
+
+    private void Update()
+    {
+        if (!tutorialRoom.gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator changeText()
+    {
+        yield return new WaitForSeconds(6.0f);
+        tutorialText.text = "";
+        yield return new WaitForSeconds(1.0f);
+        tutorialText.text = "Press left mouse button to shoot";
+        yield return new WaitForSeconds(4.0f);
+        tutorialText.text = "Try it out on the training dummies!";
+        yield return new WaitForSeconds(3.0f);
+        tutorialText.text = "";
+        yield return new WaitForSeconds(1.0f);
+        tutorialText.text = "Press right mouse button to use Shadowstep";
+        yield return new WaitForSeconds(5.0f);
+        tutorialText.text = "";
+        yield return new WaitForSeconds(1.0f);
+        tutorialText.text = "Press space button to dash";
+        yield return new WaitForSeconds(5.0f);
+        tutorialText.text = "Beware of ability cooldowns";
+        yield return new WaitForSeconds(3.0f);
+        tutorialText.text = "";
+        yield return new WaitForSeconds(1.0f);
+        tutorialText.text = "Active portals are blue, use them to proceed";
+    }
+}
