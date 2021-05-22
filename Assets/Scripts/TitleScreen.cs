f27ac309ca8922e6ceb267463de11dcb0f1db2c8
stjerndale <stjernda@kth.se>
2021-05-22 12:29:02 +0200

    Add lore text to title screen
    Added a short text giving the context/story of the game to the title screen
    Moved stuff around a little on the title screen to make room for it
    Added the loertext to the fadeaway script so it fades just like the rest of the title screen.

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -8,9 +8,8 @@ using UnityEngine.Audio;
 
 public class TitleScreen : MonoBehaviour
 {
-
-
     [SerializeField] private TMP_Text playText;
+    [SerializeField] private TMP_Text loreText;
     [SerializeField] private Image logo;
     [SerializeField] private Image bg;
     private float t = 0;
@@ -37,7 +36,7 @@ public class TitleScreen : MonoBehaviour
         {
             bounceable = false;
             playText.rectTransform.localScale = new Vector3(1.6f, 1.6f, 1);
-            playText.rectTransform.localPosition = new Vector3(39, -220, 1);
+            playText.rectTransform.localPosition = new Vector3(39, -338, 1);
             StartCoroutine(fade());
             StartCoroutine(fadeMusic(0.5f));
             Invoke("startGame", 0.5f);
@@ -63,7 +62,7 @@ public class TitleScreen : MonoBehaviour
             t = 0;
         }
         playText.rectTransform.localScale = ((1 - t) * new Vector3(1.3f, 1.3f, 1)) + (t * new Vector3(1.4f, 1.4f, 1));
-        playText.rectTransform.localPosition = ((1 - t) * new Vector3(39, -228, 1)) + (t * new Vector3(39, -223, 1));
+        playText.rectTransform.localPosition = ((1 - t) * new Vector3(39, -345, 1)) + (t * new Vector3(39, -341, 1));
     }
 
     private void startGame()
@@ -79,9 +78,11 @@ public class TitleScreen : MonoBehaviour
         Color logoColor = logo.color;
         Color bgColor = bg.color;
         Color textColor = playText.color;
+        Color loreTextColor = loreText.color;
         for (float i = 1; i < 11; i++)
         {
             playText.color = textColor - new Color(0, 0, 0, i / 5f);
+            loreText.color = loreTextColor - new Color(0, 0, 0, i / 5f);
             logo.color = logoColor - new Color(0, 0, 0, i/10f);
             bg.color = bgColor - new Color(0, 0, 0, i/10f);
             yield return new WaitForSeconds(0.05f);
